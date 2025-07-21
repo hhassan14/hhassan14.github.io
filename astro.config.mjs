39c4a18ceeb62034041064ec14ab84685530cc15
hhassan14 <onceastarrynight@gmail.com>
2025-07-20 17:16:32 -0700

    asf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hhassan14.github.io',
+  base: '/meowmeowow.github.io',
+  output: 'static'
+});
